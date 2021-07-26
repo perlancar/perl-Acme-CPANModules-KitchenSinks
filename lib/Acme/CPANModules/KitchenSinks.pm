6b7d8209b7cc332d53f89541b7117b708586e2c2
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-26 07:36:47 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/KitchenSinks.pm
@@ -0,0 +1,31 @@
+package Acme::CPANModules::KitchenSinks;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use strict;
+use warnings;
+
+our $LIST = {
+    summary => 'Kitchen sink libraries',
+    description => <<'_',
+
+A "kitchen sink" module is a module that tries to provide all sorts of
+functions/methods for various things. It often starts as a developer's personal
+"general utilities" library that grows and grows to the point of "it should
+probably be refactored into multiple modules (but isn't yet)". Often it also
+contains functionalities that are already present in other modules, but added
+into the module anyway because it is convenient for the developer.
+
+This list catalogs modules that I think are kitchen sink libraries.
+
+_
+    entries => [
+        {module=>'Data::Table::Text'},
+    ],
+};
+
+1;
+# ABSTRACT:
